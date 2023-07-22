dbf998b0c50b2dbd5c0aac2de73cd53b9bd4eb19
KhaledAchech <achechkhaled@gmail.com>
2023-07-22 23:56:46 +0200

    fixed repo annotation

--- scuttle-back/src/main/java/com/khaledachech/scuttleback/repository/storage/DocumentRepository.java
+++ scuttle-back/src/main/java/com/khaledachech/scuttleback/repository/storage/DocumentRepository.java
@@ -2,6 +2,8 @@ package com.khaledachech.scuttleback.repository.storage;
 
 import com.khaledachech.scuttleback.model.storage.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DocumentRepository extends JpaRepository<Document, Long> {
 }
--- scuttle-back/src/main/java/com/khaledachech/scuttleback/repository/storage/ProjectRepository.java
+++ scuttle-back/src/main/java/com/khaledachech/scuttleback/repository/storage/ProjectRepository.java
@@ -2,6 +2,8 @@ package com.khaledachech.scuttleback.repository.storage;
 
 import com.khaledachech.scuttleback.model.storage.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 }
